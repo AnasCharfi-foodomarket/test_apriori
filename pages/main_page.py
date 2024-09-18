68767cf884cf6296c5434b4639e792d013a209d0
Anas Charfi <48163447+AnasCharfi@users.noreply.github.com>
2024-09-18 15:26:47 +0100

    added files

---
+++ pages/main_page.py
@@ -0,0 +1,101 @@
+import streamlit as st
+from download import download_data
+from preprocess import load_and_preprocess, process_data
+from apriori_analysis import apriori_rules
+import pandas as pd
+import openpyxl
+
+def main_page():
+    st.title("Analyse de Produits Complémentaires")
+
+    file_urls = {
+        'FR': 'https://drive.google.com/uc?id=1sv6E1UsMV3fe-T_3p94uAUt1kz4xlXZA',
+        'Belgium': 'https://drive.google.com/uc?id=1fqu_YgsovkDrpqV7OsFStusEvM-9axRg',
+        'UK': 'https://drive.google.com/uc?id=1ROT0ide8EQfgcWpXMY6Qnyp5nMKoLt-a',
+        'US': 'https://drive.google.com/uc?id=1HsxBxGpq3lSwJKPALDsDNvJXNi6us2j-'
+    }
+
+    output_paths = {
+        'FR': 'dataFR.xlsx',
+        'Belgium': 'dataBE.xlsx',
+        'UK': 'dataUK.xlsx',
+        'US': 'dataUS.xlsx'
+    }
+
+    user_country = st.selectbox("Choisissez le pays à analyser :", options=list(file_urls.keys()))
+
+    file_url = file_urls[user_country]
+    output_path = output_paths[user_country]
+
+    st.write(f"Téléchargement des données pour {user_country}...")
+    data = download_data(file_url, output_path)
+
+    # Vérification si les données ont bien été chargées
+    if data.empty:
+        st.error("Les données n'ont pas été correctement chargées pour ce pays.")
+        return
+    
+    st.write("Données chargées :", data.head())
+
+    # Vérification des colonnes obligatoires
+    required_columns = ['region', 'Product Category', 'product_name']
+    missing_columns = [col for col in required_columns if col not in data.columns]
+    if missing_columns:
+        st.error(f"Les colonnes suivantes sont manquantes : {', '.join(missing_columns)}")
+        return
+
+    if user_country in ['FR', 'US']:
+        zone_mappings = {
+            'FR': ['Toute France', 'Paris', 'Paris EST', 'Paris Ouest', 'Province'],
+            'US': ['All US', 'CA', 'NY']
+        }
+        user_region = st.selectbox("Choisissez la région à analyser :", options=zone_mappings[user_country])
+        if user_region in ['Toute France', 'All US']:
+            data_region_selected = data
+        else:
+            data_region_selected = data[data['region'] == user_region]
+    else:
+        user_region = user_country
+        data_region_selected = data
+
+    st.write("Données pour la région sélectionnée :", data_region_selected.head())
+
+    if data_region_selected.empty:
+        st.error("Aucune donnée disponible pour cette région.")
+    else:
+        categories = data_region_selected['Product Category'].unique()
+        chosen_category = st.selectbox("Choisissez la catégorie pour l'analyse :", options=categories)
+
+        top_products = data_region_selected[data_region_selected['Product Category'] == chosen_category]['product_name'].value_counts().index.tolist()
+        if not top_products:
+            st.error("Aucun produit disponible pour cette catégorie.")
+        else:
+            chosen_product = st.selectbox("Choisissez le produit pour l'analyse :", options=top_products)
+
+            if st.button("Lancer l'analyse"):
+                category_data = data_region_selected[data_region_selected['Product Category'] == chosen_category].copy()
+                assert 'Date' in category_data.columns, "La colonne 'Date' n'est pas dans category_data"
+                transactions = process_data(category_data)
+                rules = apriori_rules(transactions)
+                
+                # Prétraitement des règles pour affichage
+                rules['antecedents'] = rules['antecedents'].apply(lambda x: list(x))
+                rules['consequents'] = rules['consequents'].apply(lambda x: list(x))
+                rules_single = rules[(rules['antecedents'].apply(len) == 1) & (rules['consequents'].apply(len) == 1)]
+                rules_single['antecedents'] = rules_single['antecedents'].apply(lambda x: ', '.join(x))
+                rules_single['consequents'] = rules_single['consequents'].apply(lambda x: ', '.join(x))
+                rules_single[['antecedent support', 'consequent support', 'support', 'confidence', 'lift', 'leverage', 'conviction']] = \
+                    rules_single[['antecedent support', 'consequent support', 'support', 'confidence', 'lift', 'leverage', 'conviction']].round(2)
+                rules_high_support = rules_single[rules_single['antecedent support'] > 0.1]
+                rules_chosen_product = rules_single[rules_single['antecedents'] == chosen_product]
+                
+                # Affichage des règles
+                columns_to_display = ['antecedents', 'consequents', 'antecedent support', 'consequent support',
+                                      'support', 'confidence', 'lift', 'leverage', 'conviction']
+                rules_display = rules_chosen_product[columns_to_display]
+                rules_display.columns = ['Antécédents', 'Conséquents', 'Support Antécédent', 'Support Conséquent',
+                                         'Support', 'Confiance', 'Lift', 'Leverage', 'Conviction']
+                rules_display.sort_values(by=['Leverage', 'Support Antécédent', 'Confiance', 'Lift'], ascending=[False, False, False, False], inplace=True)
+                
+                st.subheader("Résultats de l'Analyse")
+                st.dataframe(rules_display.head(50))
---
+++ pages/segmentation_page.py
@@ -0,0 +1,62 @@
+import streamlit as st
+from download import download_data, download_segmentation_data
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def segmentation_page():
+    st.title("Segmentation des Clients")
+    segmentation_url = 'https://docs.google.com/spreadsheets/d/1lkxXgC095L0OQdItsGYtisZqVk_nXkPa/export?format=xlsx'
+    segmentation_output_path = 'segmentationFR.xlsx'
+    segmentation_data = download_segmentation_data(segmentation_url, segmentation_output_path)
+    zones = ['Toute France', 'Paris', 'Paris EST', 'Paris Ouest', 'Province']
+    selected_zone = st.selectbox("Choisissez la zone :", options=zones)
+    categories = ['Fruits et Légumes', 'Boucherie', 'Epicerie salée', 'Crémerie', 'Toutes catégories']
+    selected_category = st.selectbox("Choisissez la catégorie :", options=categories)
+    months = ['avril', 'mai', 'juin', 'juillet', '3 last months']
+    selected_month = st.selectbox("Choisissez le mois :", options=months)
+    
+    if st.button("Lancer l'analyse"):
+        file_urls = {
+            'France': 'https://docs.google.com/spreadsheets/d/1_qfuH19aLu3JMocit2-wSL-FCmWNet1I/export?format=xlsx'
+        }
+        output_paths = {
+            'France': 'dataFR.xlsx'
+        }
+        france_data = download_data(file_urls['France'], output_paths['France'])
+        france_data['Date'] = pd.to_datetime(france_data['Date'], errors='coerce')
+        st.write("Colonnes dans france_data:", france_data.columns)
+        st.write("Colonnes dans segmentation_data:", segmentation_data.columns)
+        
+        if selected_zone != 'Toute France':
+            france_data = france_data[france_data['region'] == selected_zone]
+        if selected_category != 'Toutes catégories':
+            france_data = france_data[france_data['Product Category'] == selected_category]
+        
+        if selected_month == 'avril':
+            france_data = france_data[france_data['Date'].dt.month == 4]
+        elif selected_month == 'mai':
+            france_data = france_data[france_data['Date'].dt.month == 5]
+        elif selected_month == 'juin':
+            france_data = france_data[france_data['Date'].dt.month == 6]
+        elif selected_month == 'juillet':
+            france_data = france_data[france_data['Date'].dt.month == 7]
+        elif selected_month == '3 last months':
+            available_months = france_data['Date'].dt.month.unique()
+            if 7 in available_months:
+                france_data = france_data[france_data['Date'].dt.month.isin([5, 6, 7])]
+            else:
+                france_data = france_data[france_data['Date'].dt.month.isin([4, 5, 6])]
+
+        merged_data = france_data.merge(segmentation_data, left_on='client_id', right_on='Restaurant_id')
+        segment_counts = merged_data.groupby(['Gamme', 'Type']).size().unstack(fill_value=0)
+        plt.figure(figsize=(10, 6))
+        sns.heatmap(segment_counts, annot=True, fmt="d", cmap="YlGnBu")
+        plt.title("Heatmap du nombre de clients pour chaque segment")
+        plt.xlabel("Type")
+        plt.ylabel("Gamme")
+        st.pyplot(plt)
+
+# Assurez-vous d'exécuter la fonction pour afficher la page de segmentation
+if __name__ == '__main__':
+    segmentation_page()
